21d97b1235b50f531ac8feda14df061801ec28a8
LeiSuCN <s.lei1116@163.com>
2016-06-11 23:08:59 +0800

    form

--- src/components/Notices/NoticeEditor.jsx
+++ src/components/Notices/NoticeEditor.jsx
@@ -1,22 +1,41 @@
 import React, { Component, PropTypes } from 'react';
-import { Form, Input, Button, Icon, Select, Radio,Upload, Row, Col,Spin } from 'antd';
+import { Form, Input, InputNumber, Button, Icon, Select, Radio,Upload, Row, Col,Spin } from 'antd';
+import ifetch from 'isomorphic-fetch';
 import MainLayout from '../../layouts/MainLayout/MainLayout';
+
+
 
 const FormItem = Form.Item;
 const RadioGroup = Radio.Group;
+const Option = Select.Option;
 
 class NoticeEditor extends Component {
 
+  constructor(props){
+    super();
+    this.state = {};
+  }
+
   render() {
+    const { getFieldProps, getFieldError, isFieldValidating } = this.props.form;
+   
+    let titleProps = getFieldProps('title', {
+      rules:[
+        { required: true, min: 4, message: '标题至少为4个字'}
+      ]
+    });
+    
+    
     return (
-      <Form horizontal>
+      <Form horizontal form={this.props.form}>
          <Row>
           <Col span="24">
             <FormItem
               label="主题"
               labelCol={{span:1}}
-              wrapperCol={{span:23}}>
-                <Input />
+              wrapperCol={{span:23}}
+              hasFeedback>
+                <Input {...titleProps} name='title' />
             </FormItem>
   	      </Col>
         </Row> 
@@ -57,7 +76,7 @@ class NoticeEditor extends Component {
               label="附件"
               labelCol={{span:1}}
               wrapperCol={{span:23}}>
-              <Upload>
+              <Upload action="http://mw-weboa.oss-cn-shenzhen.aliyuncs.com"  data={this.uploadFileWithKey.bind(this)} beforeUpload={this.getFileKey.bind(this)}  >
                 <Button type="ghost">
                   <Icon type="upload" />点击上传
                </Button>
@@ -65,7 +84,7 @@ class NoticeEditor extends Component {
             </FormItem>
          </Col>
         </Row>
-        <Row>
+         <Row>
           <Col span="24">
              <FormItem
               label="重要度"
@@ -79,10 +98,79 @@ class NoticeEditor extends Component {
             </FormItem>
          </Col>
         </Row>
+         <Row>
+          <Col span="24">
+             <FormItem
+              label="有效期"
+              labelCol={{span:1}}
+              wrapperCol={{span:23}}>
+              <InputNumber min={0} defaultValue={0} />天（为0或空则永远不过期）
+            </FormItem>
+         </Col>
+        </Row>
+        <Row>
+          <Col span="24">
+            <Button onClick={this.commit.bind(this)}   htmlType='submit'>提交</Button>
+         </Col>
+        </Row>
       </Form>
     )
   }
+
+  uploadFileWithKey(file) {
+
+    if( this.state.filetoken ){
+      console.log( this.state.filetoken );
+      this.state.filetoken.key = '123123123';
+      return this.state.filetoken;
+    }
+  }
+
+  getFileKey(file) {
+
+    if( this.state.filetoken ) {
+      return true;
+    } else{
+    return new Promise( (resolve, reject) => {
+      //ifetch('//file.weboa.leisucn.com/token/upload',{mode:'cors'}).then(
+      ifetch('//localhost:38080/token/upload',{mode:'cors'}).then(
+        resp => {
+          let json = resp.json();
+          if( json && json.then ){
+            json.then( _json => { this.setFileToken(_json); resolve() } );
+          } else if( json ){
+            this.setFileToken(json);
+            resolve();
+          }
+        } 
+      )
+   })}
+  }
+
+  setFileToken(resp){
+    if( !this.state.filetoken ) this.state.filetoken = {};
+    this.state.filetoken["success_action_status"] = 200;
+    this.state.filetoken.policy = resp.policy;
+    this.state.filetoken.OSSAccessKeyId = resp.accessid;
+    this.state.filetoken.signature =resp.signature;
+  }
+
+  commit(e){
+    e.preventDefault();
+    let form = this.props.form;
+    form.validateFields( (errors, values) => {
+      console.log( errors )
+      console.log( values )
+    })
+  }
+
+  builderOrder() {
+
+
+  }
 }
+
+NoticeEditor = Form.create()(NoticeEditor); 
 
 class NoticeEditorContainer extends Component {
 
