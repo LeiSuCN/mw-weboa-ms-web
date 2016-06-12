b75a90e6d507f19d9dc51b59f3f0a20ac1d50040
LeiSuCN <s.lei1116@163.com>
2016-06-12 22:59:01 +0800

    validate

--- src/components/Notices/NoticeEditor.jsx
+++ src/components/Notices/NoticeEditor.jsx
@@ -1,7 +1,10 @@
 import React, { Component, PropTypes } from 'react';
 import { Form, Input, InputNumber, Button, Icon, Select, Radio,Upload, Row, Col,Spin } from 'antd';
 import ifetch from 'isomorphic-fetch';
+import {Editor, EditorState} from 'draft-js';
+import {stateToHTML} from 'draft-js-export-html';
 import MainLayout from '../../layouts/MainLayout/MainLayout';
+import styles from './NoticeEditor.less';
 
 
 
@@ -12,8 +15,9 @@ const Option = Select.Option;
 class NoticeEditor extends Component {
 
   constructor(props){
-    super();
+    super(props);
     this.state = {};
+    this.state.editorState = EditorState.createEmpty();
   }
 
   render() {
@@ -24,10 +28,34 @@ class NoticeEditor extends Component {
         { required: true, min: 4, message: '标题至少为4个字'}
       ]
     });
-    
+
+    let cateProps = getFieldProps('category', {
+      rules:[
+        { required: true, message: '请选择类别'}
+      ]
+    });
+
+     let receiverProps = getFieldProps('receiver', {
+      rules:[
+        { required: true, message: '请选择部门'}
+      ]
+    });
+
+     let typeProps = getFieldProps('type', {
+      initialValue: "3",
+      rules:[
+        { required: true, message: '请选择重要度'}
+      ]
+    });
+
+   let validityProps = getFieldProps('validity', {
+      initialValue: "0"
+    });
+
+   
     
     return (
-      <Form horizontal form={this.props.form}>
+      <Form horizontal form={this.props.form} className={styles.notice_editor}>
          <Row>
           <Col span="24">
             <FormItem
@@ -45,7 +73,7 @@ class NoticeEditor extends Component {
               label="类别"
               labelCol={{span:2}}
               wrapperCol={{span:22}}>
-              <Select showSearch notFoundContent="无法找到">
+              <Select showSearch notFoundContent="无法找到" {...cateProps}  name="category">
                 <Option value="1">通知通告</Option>
                 <Option value="2">通告通知</Option>
                 <Option value="3">系统消息</Option>
@@ -57,7 +85,7 @@ class NoticeEditor extends Component {
               label="部门"
               labelCol={{span:2}}
               wrapperCol={{span:22}}>
-              <Select showSearch notFoundContent="无法找到">
+              <Select showSearch notFoundContent="无法找到" {...receiverProps} name="receiver">
                 <Option value="1">财务</Option>
                 <Option value="2">人事</Option>
                 <Option value="3">IT</Option>
@@ -67,7 +95,9 @@ class NoticeEditor extends Component {
         </Row> 
         <Row>
           <Col span="24">
-            <div style={{ height: '20em', backgroundColor:'white'}}></div>
+            <div className = {styles.notice_content} >
+              <Editor editorState={this.state.editorState} placeholder1="请输入通知内容" onChange={this.editorChange.bind(this)} blockStyleFn={ () => styles.notice_content_block }  />
+            </div>
           </Col>
         </Row>
          <Row>
@@ -76,7 +106,7 @@ class NoticeEditor extends Component {
               label="附件"
               labelCol={{span:1}}
               wrapperCol={{span:23}}>
-              <Upload action="http://mw-weboa.oss-cn-shenzhen.aliyuncs.com"  data={this.uploadFileWithKey.bind(this)} beforeUpload={this.getFileKey.bind(this)}  >
+              <Upload action="http://mw-weboa.oss-cn-shenzhen.aliyuncs.com"  data={this.uploadFileWithKey.bind(this)} onChange={this.uploadSucc.bind(this)}  beforeUpload={this.getFileKey.bind(this)}  >
                 <Button type="ghost">
                   <Icon type="upload" />点击上传
                </Button>
@@ -90,7 +120,7 @@ class NoticeEditor extends Component {
               label="重要度"
               labelCol={{span:1}}
               wrapperCol={{span:23}}>
-              <RadioGroup>
+              <RadioGroup {...typeProps} name="type">
                 <Radio value="1">非常重要</Radio>
                 <Radio value="2">重要</Radio>
                 <Radio value="3">普通</Radio>
@@ -104,7 +134,7 @@ class NoticeEditor extends Component {
               label="有效期"
               labelCol={{span:1}}
               wrapperCol={{span:23}}>
-              <InputNumber min={0} defaultValue={0} />天（为0或空则永远不过期）
+              <InputNumber {...validityProps} min={0} defaultValue={0} />天（为0或空则永远不过期）
             </FormItem>
          </Col>
         </Row>
@@ -118,10 +148,9 @@ class NoticeEditor extends Component {
   }
 
   uploadFileWithKey(file) {
-
+    console.log( file )
     if( this.state.filetoken ){
-      console.log( this.state.filetoken );
-      this.state.filetoken.key = '123123123';
+      this.state.filetoken.key = file.uid;
       return this.state.filetoken;
     }
   }
@@ -155,18 +184,46 @@ class NoticeEditor extends Component {
     this.state.filetoken.signature =resp.signature;
   }
 
+  editorChange(editorState) {
+    this.state.editorState = editorState;
+    this.setState(this.state)
+  }
+
   commit(e){
     e.preventDefault();
+    let notice = false;
     let form = this.props.form;
     form.validateFields( (errors, values) => {
       console.log( errors )
+
+      if( errors ) {
+        return ;
+      }
       console.log( values )
+
+       notice = Object.assign({ }, values);
     })
+
+    if( !notice ) {
+      return
+    }
+
+    var content = this.state.editorState.getCurrentContent();
+
+    if( content.hasText() ) {
+      notice.content = stateToHTML(content);
+    }
+    console.log( notice );
   }
 
   builderOrder() {
 
 
+  }
+
+  uploadSucc(ret, file) {
+    console.log( ret )
+    console.log( file )
   }
 }
 
